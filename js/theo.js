2cbcc873e80b71b8003a53f28d8f73a66f876ec5
lokrainmyth <f2kyy28kts@privaterelay.appleid.com>
2026-07-07 02:33:18 +0700

    Create theo.js

---
+++ js/theo.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const Theo = {
+
+    scene:null,
+
+    camera:null,
+
+    renderer:null,
+
+    model:null,
+
+    init(){
+
+        console.log("Theo initialized");
+
+    }
+
+};
