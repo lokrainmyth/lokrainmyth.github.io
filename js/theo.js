1f782b1f8c3b3a0e191423346f87ade968fcb102
lokrainmyth <f2kyy28kts@privaterelay.appleid.com>
2026-07-14 00:48:06 +0700

    Update theo.js

--- js/theo.js
+++ js/theo.js
@@ -360,6 +360,62 @@ Math.sin(performance.now()*0.00018)*0.18;
 
     }
 
+function createDustTexture() {
+
+    const size = 128;
+
+    const canvas = document.createElement("canvas");
+
+    canvas.width = size;
+    canvas.height = size;
+
+    const ctx = canvas.getContext("2d");
+
+    const gradient = ctx.createRadialGradient(
+
+        size / 2,
+        size / 2,
+        0,
+
+        size / 2,
+        size / 2,
+        size / 2
+
+    );
+
+    gradient.addColorStop(
+        0,
+        "rgba(255,255,255,1)"
+    );
+
+    gradient.addColorStop(
+        0.25,
+        "rgba(255,255,255,.35)"
+    );
+
+    gradient.addColorStop(
+        0.6,
+        "rgba(255,255,255,.08)"
+    );
+
+    gradient.addColorStop(
+        1,
+        "rgba(255,255,255,0)"
+    );
+
+    ctx.fillStyle = gradient;
+
+    ctx.fillRect(
+        0,
+        0,
+        size,
+        size
+    );
+
+    return new THREE.CanvasTexture(canvas);
+
+}
+    
     function createDust() {
 
     const geometry =
