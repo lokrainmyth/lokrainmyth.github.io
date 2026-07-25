6e6a9419089002e06bdc7d562b98c0c045a7b791
lokrainmyth <f2kyy28kts@privaterelay.appleid.com>
2026-07-25 19:16:33 +0700

    Update myth.js

--- js/myth.js
+++ js/myth.js
@@ -9,6 +9,11 @@ function init(){
 const svg =
 document.querySelector("#mythMap svg");
 
+const path =
+document.getElementById("pathTheoDreamer");
+
+path.style.strokeDashoffset = 0;
+
 if(!object)return;
 
 object.addEventListener(
