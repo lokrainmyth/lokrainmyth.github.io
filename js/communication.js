508a152993ae3fc3d87f8531f38b4724ef9578d4
lokrainmyth <f2kyy28kts@privaterelay.appleid.com>
2026-08-15 05:53:08 +0700

    Create communication.js

---
+++ js/communication.js
@@ -0,0 +1,82 @@
+"use strict";
+
+
+document.addEventListener(
+"DOMContentLoaded",
+()=>{
+
+
+emailjs.init(
+    "ERryTDaq2vefa_Fop"
+);
+
+
+const form =
+document.getElementById(
+    "communicationForm"
+);
+
+
+if(!form) return;
+
+
+form.addEventListener(
+"submit",
+function(event){
+
+    event.preventDefault();
+
+
+    const params = {
+
+        from_name:
+        document.getElementById(
+            "senderName"
+        ).value,
+
+
+        contact:
+        document.getElementById(
+            "senderContact"
+        ).value,
+
+
+        message:
+        document.getElementById(
+            "message"
+        ).value
+
+    };
+
+
+    emailjs.send(
+        "service_x28p0ve",
+        "template_7x2kupb",
+        params
+    )
+    .then(()=>{
+
+        alert(
+        "Thank you. Your message has reached Lo.Krain."
+        );
+
+
+        form.reset();
+
+
+    })
+    .catch(error=>{
+
+        console.log(error);
+
+        alert(
+        "Something went wrong."
+        );
+
+    });
+
+
+});
+
+
+});
