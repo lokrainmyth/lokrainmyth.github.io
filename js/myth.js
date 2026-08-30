bb2f63ce9a8b6490a7cc4b665e04f373dea4020f
lokrainmyth <f2kyy28kts@privaterelay.appleid.com>
2026-08-30 17:55:26 +0700

    Update myth.js

--- js/myth.js
+++ js/myth.js
@@ -30,193 +30,271 @@ const Myth = {
 
         this.viewport.innerHTML = `
 
-    <svg class="myth-svg" viewBox="0 0 1200 800">
+   <svg class="myth-svg" viewBox="0 0 1200 800">
 
-        <!-- paths -->
-        <g class="paths">
+    <!-- paths -->
+    <g class="paths">
 
-            <path id="path1"
+        <path id="pathHorizontal"
             d="M120 650 C260 560 390 500 520 410"/>
 
-            <path id="path2"
+        <path id="pathDown"
             d="M520 410 C650 330 720 350 790 420"/>
 
-            <path id="path3"
+        <path id="pathVertical"
             d="M790 420 C900 300 960 210 1040 150"/>
 
-        </g>
+    </g>
 
 
-        <!-- THEO -->
-        <g class="node theo">
 
-            <circle class="pulse"
+    <!-- THEO -->
+
+    <g class="node theo"
+       data-myth-action="expulsion">
+
+        <circle class="pulse"
             cx="120"
             cy="650"
             r="7"/>
 
-            <text x="145" y="660">
-                THEO
-            </text>
+
+        <text x="145" y="660">
+            THEO
+        </text>
 
 
-            <!-- scarecrow -->
-            <g class="ghost scarecrow">
+        <!-- scarecrow -->
 
-                <line x1="120" y1="610" x2="120" y2="560"/>
-                <line x1="90" y1="585" x2="150" y2="585"/>
+        <g class="ghost scarecrow">
 
-                <circle cx="120" cy="545" r="15"/>
+            <path d="
+                M90 520
+                Q120 500 150 520
+                L145 535
+                L95 535 Z"/>
 
-                <path d="
-                M90 530
-                Q120 510 150 530
-                L145 520
-                L95 520 Z"/>
 
-            </g>
+            <circle
+                cx="120"
+                cy="550"
+                r="14"/>
+
+
+            <line
+                x1="120"
+                y1="565"
+                x2="120"
+                y2="620"/>
+
+
+            <line
+                x1="90"
+                y1="585"
+                x2="150"
+                y2="585"/>
 
         </g>
 
+    </g>
 
 
-        <!-- DREAMER -->
 
-        <g class="node dreamer hidden">
 
-            <circle class="dot"
+
+    <!-- DREAMER -->
+
+    <g class="node dreamer hidden"
+       data-myth-action="dreamer">
+
+
+        <circle class="dot"
             cx="520"
             cy="410"
             r="6"/>
 
-            <text x="545" y="420">
-                DREAMER
-            </text>
+
+        <text x="545" y="420">
+            DREAMER
+        </text>
 
 
-            <!-- sitting boy -->
 
-            <g class="ghost boy">
+        <!-- sitting boy -->
 
-                <circle cx="520" cy="455" r="12"/>
+        <g class="ghost boy">
 
-                <path d="
-                M520 470
-                C500 490 510 530 550 540
+
+            <circle
+                cx="520"
+                cy="465"
+                r="12"/>
+
+
+            <path d="
+                M520 478
+                C500 500 510 535 550 545
                 M510 520
-                L480 560
-                M540 520
-                L580 540
-                "/>
+                L470 555
+                M540 525
+                L590 555
+            "/>
 
-                <path d="
+
+            <path d="
                 M520 455
-                Q540 445 550 455"/>
+                Q540 445 550 455
+            "/>
 
-            </g>
 
         </g>
 
 
+    </g>
 
 
-        <!-- DARKEST -->
 
-        <g class="node darkest hidden">
 
-            <circle class="dot"
+
+
+    <!-- THE DARKEST HOUR -->
+
+    <g class="node darkest hidden"
+       data-myth-action="darkest">
+
+
+        <circle class="dot"
             cx="790"
             cy="420"
             r="6"/>
 
 
-            <text x="815" y="430">
-                THE DARKEST HOUR
+        <text x="815" y="430">
+            THE DARKEST HOUR
+        </text>
+
+
+
+        <g class="stars">
+
+            <text x="760" y="500">
+                ★
             </text>
 
 
-            <g class="stars">
+            <text x="820" y="550">
+                ★
+            </text>
 
-                <text x="760" y="500">★</text>
-                <text x="820" y="550">★</text>
-                <text x="900" y="510">★</text>
 
-            </g>
+            <text x="900" y="510">
+                ★
+            </text>
+
 
         </g>
 
 
+    </g>
 
 
-        <!-- IO -->
 
-        <g class="node io hidden">
 
-            <circle class="pulse io-dot"
+
+
+
+    <!-- IO -->
+
+    <g class="node io hidden"
+       data-myth-action="dawn">
+
+
+        <circle class="pulse io-dot"
             cx="1040"
             cy="150"
             r="7"/>
 
 
-            <!-- sun -->
 
-            <g class="sun">
+        <!-- SUN -->
 
-                <circle cx="1040" cy="150" r="35"/>
+        <g class="sun">
 
-                <path d="
+            <circle
+                cx="1040"
+                cy="150"
+                r="35"/>
+
+
+            <path d="
                 M1040 95V75
                 M1040 205V225
+
                 M985 150H965
                 M1095 150H1115
+
                 M1000 110L985 95
                 M1080 190L1095 205
+
                 M1080 110L1095 95
                 M1000 190L985 205
-                "/>
+            "/>
 
-            </g>
-
-
-
-            <text x="1090" y="150">
-                IO
-            </text>
-
-            <text class="subtitle"
-            x="1090"
-            y="185">
-                The Dawn has come
-            </text>
-
-
-
-            <!-- birds -->
-
-            <g class="birds">
-
-                <path d="
-                M1110 90
-                q20 -20 40 0
-                q20 -20 40 0"/>
-
-                <path d="
-                M1140 120
-                q20 -20 40 0
-                q20 -20 40 0"/>
-
-                <path d="
-                M1170 150
-                q20 -20 40 0
-                q20 -20 40 0"/>
-
-            </g>
 
         </g>
 
 
 
-    </svg>
+
+
+        <text x="1090" y="150">
+            IO
+        </text>
+
+
+        <text class="subtitle"
+              x="1090"
+              y="185">
+            The Dawn has come
+        </text>
+
+
+
+
+
+        <!-- birds -->
+
+        <g class="birds">
+
+
+            <path d="
+                M1110 90
+                q20 -20 40 0
+                q20 -20 40 0
+            "/>
+
+
+            <path d="
+                M1140 120
+                q20 -20 40 0
+                q20 -20 40 0
+            "/>
+
+
+            <path d="
+                M1170 150
+                q20 -20 40 0
+                q20 -20 40 0
+            "/>
+
+
+        </g>
+
+
+    </g>
+
+
+</svg>
 
 `;
 
