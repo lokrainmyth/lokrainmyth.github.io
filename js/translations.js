8e19428e8328aba97aefd5878c66d493ebe602d8
lokrainmyth <f2kyy28kts@privaterelay.appleid.com>
2026-09-12 00:47:36 +0700

    Update translations.js

--- js/translations.js
+++ js/translations.js
@@ -5,13 +5,13 @@ const translations = {
     en: {
 
         albumDescription:
-`An album completing a fifteen-year journey from night to dawn. Start listening and head into the night, track by track, <span class="hiddenClear">
+`An album completing a fifteen-year journey from night to dawn. Start listening and head into the night, track by track, <span class="pathReveal">
 <span>u</span><span>n</span><span>t</span><span>i</span><span>l</span><span>&nbsp;</span>
 <span>t</span><span>h</span><span>e</span><span>&nbsp;</span>
-<span>h</span><span>i</span><span>d</span><span>d</span><span>e</span><span>n</span><span>&nbsp;</span>
-<span>b</span><span>e</span><span>c</span><span>o</span><span>m</span><span>e</span><span>s</span><span>&nbsp;</span>
-<span>c</span><span>l</span><span>e</span><span>a</span><span>r</span>
-</span>.`,
+<span>p</span><span>a</span><span>t</span><span>h</span><span>&nbsp;</span>
+<span>r</span><span>e</span><span>v</span><span>e</span><span>a</span><span>l</span><span>s</span><span>&nbsp;</span>
+<span>i</span><span>t</span><span>s</span><span>e</span><span>l</span><span>f</span><span>.</span>
+</span>` ,
 
         about:
         "About",
@@ -64,7 +64,13 @@ messageSent:
     ru: {
 
         albumDescription:
-        "Альбом, завершающий пятнадцатилетний путь от ночи к рассвету. Начни слушать и отправляйся в ночь — трек за треком, — пока скрытое не станет явным.",
+`Альбом, завершающий пятнадцатилетний путь от ночи к рассвету. Начни слушать и отправляйся в ночь — трек за треком, <span class="pathReveal">
+<span>п</span><span>о</span><span>к</span><span>а</span><span>&nbsp;</span>
+<span>п</span><span>у</span><span>т</span><span>ь</span><span>&nbsp;</span>
+<span>н</span><span>е</span><span>&nbsp;</span>
+<span>о</span><span>т</span><span>к</span><span>р</span><span>о</span><span>е</span><span>т</span><span>&nbsp;</span>
+<span>с</span><span>а</span><span>м</span><span>.</span>
+</span>`,
 
         about:
         "О проекте",
