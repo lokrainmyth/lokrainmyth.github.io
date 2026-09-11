427db084288b325361d086884ce8b7d05d9c50fd
lokrainmyth <f2kyy28kts@privaterelay.appleid.com>
2026-09-12 00:27:24 +0700

    Update translations.js

--- js/translations.js
+++ js/translations.js
@@ -5,13 +5,7 @@ const translations = {
     en: {
 
         albumDescription:
-`An album completing a fifteen-year journey from night to dawn. Start listening and head into the night, track by track, <span class="hiddenClear">
-<span>u</span><span>n</span><span>t</span><span>i</span><span>l</span><span>&nbsp;</span>
-<span>t</span><span>h</span><span>e</span><span>&nbsp;</span>
-<span>h</span><span>i</span><span>d</span><span>d</span><span>e</span><span>n</span><span>&nbsp;</span>
-<span>b</span><span>e</span><span>c</span><span>o</span><span>m</span><span>e</span><span>s</span><span>&nbsp;</span>
-<span>c</span><span>l</span><span>e</span><span>a</span><span>r</span>
-</span>.`,
+'An album completing a fifteen-year journey from night to dawn. Start listening and head into the night, track by track, <span class="path-reveal">until the path reveals itself.</span>',
 
         about:
         "About",
@@ -64,7 +58,7 @@ messageSent:
     ru: {
 
         albumDescription:
-        "Альбом, завершающий пятнадцатилетний путь от ночи к рассвету. Начни слушать и отправляйся в ночь — трек за треком, — пока скрытое не станет явным.",
+'Альбом, завершающий пятнадцатилетний путь от ночи к рассвету. Начни слушать и отправляйся в ночь — трек за треком, — <span class="path-reveal">пока путь не откроется сам.</span>',
 
         about:
         "О проекте",
