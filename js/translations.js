f0bc6bba73867c5a37219890a9a0dfdef5a9a682
lokrainmyth <f2kyy28kts@privaterelay.appleid.com>
2026-09-12 00:00:15 +0700

    Update translations.js

--- js/translations.js
+++ js/translations.js
@@ -5,7 +5,13 @@ const translations = {
     en: {
 
         albumDescription:
-        "An album completing a fifteen-year journey from night to dawn. Start listening and head into the night, track by track, until the hidden becomes clear.",
+`An album completing a fifteen-year journey from night to dawn. Start listening and head into the night, track by track, <span class="hiddenClear">
+<span>u</span><span>n</span><span>t</span><span>i</span><span>l</span><span>&nbsp;</span>
+<span>t</span><span>h</span><span>e</span><span>&nbsp;</span>
+<span>h</span><span>i</span><span>d</span><span>d</span><span>e</span><span>n</span><span>&nbsp;</span>
+<span>b</span><span>e</span><span>c</span><span>o</span><span>m</span><span>e</span><span>s</span><span>&nbsp;</span>
+<span>c</span><span>l</span><span>e</span><span>a</span><span>r</span>
+</span>.`,
 
         about:
         "About",
@@ -127,8 +133,8 @@ function setLanguage(lang){
             translations[lang][key]
         ){
 
-            element.textContent =
-            translations[lang][key];
+            element.innerHTML =
+translations[lang][key];
 
         }
 
